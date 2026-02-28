b2e7ac286a4287dd782730bbb6cc2a80425a3314
Ka6e <ivan81747@gmail.com>
2026-02-28 20:13:00 +0300

    beginning of development

---
+++ src/Lexer/TokenType.cs
@@ -0,0 +1,44 @@
+﻿namespace Lexer;
+
+public enum TokenType
+{
+    /// <summary>
+    /// Ключевое слово main
+    /// </summary>
+    Main,
+
+    /// <summary>
+    /// Ключевое слово var
+    /// </summary>
+    Var,
+
+    /// <summary>
+    /// Ключевое слово const
+    /// </summary>
+    Const,
+
+    /// <summary>
+    /// Ключевое слово
+    /// </summary>
+    Read,
+
+    /// <summary>
+    /// ВЫФВ
+    /// </summary>
+    Print,
+
+    /// <summary>
+    /// 
+    /// </summary>
+    Integer,
+
+    /// <summary>
+    /// 
+    /// </summary>
+    Float,
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    String,
+}
---
+++ src/Lexer/TokenValue.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+
+namespace Lexer;
+
+public class TokenValue
+{
+    private readonly object _value;
+
+    public TokenValue(string value)
+    {
+        _value = value;
+    }
+
+    public TokenValue(decimal value)
+    {
+        _value = value;
+    }
+
+    public override string ToString()
+    {
+        return _value switch
+        {
+            string s => s,
+            decimal d => d.ToString(CultureInfo.InvariantCulture),
+            _ => throw new NotImplementedException(),
+        };
+    }
+
+    public decimal ToDecimal()
+    {
+        return _value switch
+        {
+            string s => decimal.Parse(s, CultureInfo.InvariantCulture),
+            decimal d => d,
+            _ => throw new NotImplementedException(),
+        };
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is TokenValue other)
+        {
+            return _value switch
+            {
+                string s => (string)other._value == s,
+                decimal d => (decimal)other._value == d,
+                _ => throw new NotImplementedException(),
+            };
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return _value.GetHashCode();
+    }
+}

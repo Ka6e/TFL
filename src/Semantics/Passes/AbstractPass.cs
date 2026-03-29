a6f2142492899f5813312af0c80fcaae73da4274
Ka6e <ivan81747@gmail.com>
2026-03-29 13:06:04 +0300

    started doing semantics

---
+++ src/Semantics/Passes/AbstractPass.cs
@@ -0,0 +1,12 @@
+﻿using Ast;
+using Ast.Statement;
+
+namespace Semantics.Passes;
+
+public abstract class AbstractPass : IAstVisitor
+{
+    public void Visit(AssignmentStatement s)
+    {
+        throw new NotImplementedException();
+    }
+}

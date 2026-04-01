bd7ef7a81c6abbf55b059965b0bbe1e384a21e52
raketa0 <72815676+raketa0@users.noreply.github.com>
2026-04-01 17:43:18 +0300

    Настройка проекта кодогена

---
+++ src/VirtualMachineCodegen/BasicBlock.cs
@@ -0,0 +1,7 @@
+﻿namespace VirtualMachineCodegen
+{
+    public class BasicBlock
+    {
+
+    }
+}
---
+++ src/VirtualMachineCodegen/VirtualMachineCodegen.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualMachineCodegen
+{
+    internal class VirtualMachineCodegen
+    {
+    }
+}

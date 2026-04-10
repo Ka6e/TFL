cc164ffcfdef671613800b7d8469445872863b39
raketa0 <72815676+raketa0@users.noreply.github.com>
2026-04-10 05:28:53 +0300

    конфликт слияния исправил

--- src/Interpreter/ConsoleEnvironment.cs
+++ src/Interpreter/ConsoleEnvironment.cs
@@ -1,14 +1,23 @@
-﻿using System.ComponentModel;
-using System.Globalization;
+﻿using System.Globalization;
 
-
-namespace Interpreter;
+namespace VirtualMachine;
 
 public class ConsoleEnvironment : IEnvironment
 {
+    public int ReadInt()
     {
+        string? input = Console.ReadLine();
+
+        if (int.TryParse(input, NumberStyles.Integer, CultureInfo.InvariantCulture, out int value))
+        {
+            return value;
+        }
+
+        throw new InvalidOperationException("Invalid integer input");
     }
 
+    public void PrintInt(int value)
     {
+        Console.Write(value.ToString(CultureInfo.InvariantCulture));
     }
 }

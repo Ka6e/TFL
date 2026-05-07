23ae6027338315b7b100320bce6b0bdc082052e4
raketa0 <72815676+raketa0@users.noreply.github.com>
2026-05-07 21:21:36 +0300

    обновил  InstructionsBuilder и сделал CodegenSymbolsTable

--- src/VirtualMachineCodegen/InstructionsBuilder.cs
+++ src/VirtualMachineCodegen/InstructionsBuilder.cs
@@ -9,23 +9,19 @@ public class InstructionsBuilder
 
     public InstructionsBuilder()
     {
-        _basicBlocks = [];
+        _basicBlocks = new List<BasicBlock>();
         _insertPoint = CreateBasicBlock();
     }
 
-    /// <summary>
-    /// Базовый блок, в который выполняется вставка инструкций
-    /// </summary>
     public BasicBlock InsertPoint
     {
         get => _insertPoint;
-
         set
         {
             if (!ReferenceEquals(_basicBlocks[value.Id], value))
             {
-                // Такого не должно быть по логике кодогенерации.
-                throw new InvalidOperationException("Basic block does not belong to the current instructions builder");
+                throw new InvalidOperationException(
+                    "Basic block does not belong to the current instructions builder");
             }
 
             _insertPoint = value;
@@ -38,34 +34,90 @@ public class InstructionsBuilder
     /// </summary>
     public void Append(Instruction instruction)
     {
+        if (IsJump(instruction.Code))
+        {
+            throw new InvalidOperationException(
+                $"Cannot append {instruction.Code} using this method");
+        }
+
         _insertPoint.Append(instruction);
     }
 
     /// <summary>
+    /// Добавляет инструкцию перехода на указанный базовый блок.
+    /// </summary>
+    public void AppendJump(InstructionCode code, BasicBlock target)
+    {
+        if (!IsJump(code))
+        {
+            throw new InvalidOperationException(
+                $"Instruction {code} is not a jump instruction");
+        }
+
+        _insertPoint.Append(new Instruction(code, target.Id));
+    }
+
+    /// <summary>
+    /// Создаёт базовый блок инструкций и возвращает ссылку на него.
+    /// </summary>
+    public BasicBlock CreateBasicBlock()
+    {
+        BasicBlock bb = new BasicBlock(_basicBlocks.Count);
+        _basicBlocks.Add(bb);
+        return bb;
+    }
+
+    /// <summary>
     /// Собирает финальный список инструкций из базовых блоков, заменяя адреса во всех инструкциях перехода
-    ///  на окончательные адреса инструкций.
+    /// на окончательные адреса инструкций.
     /// </summary>
     public List<Instruction> Finish()
     {
-        List<Instruction> instructions = [];
+        List<int> addresses = CalculateBasicBlockAddresses();
+        List<Instruction> instructions = new List<Instruction>();
+
         foreach (BasicBlock block in _basicBlocks)
         {
             foreach (Instruction instruction in block.Instructions)
             {
-                instructions.Add(instruction);
+                if (IsJump(instruction.Code))
+                {
+                    int newAddress = addresses[instruction.Operand.AsInt()];
+                    instructions.Add(new Instruction(instruction.Code, newAddress));
+                }
+                else
+                {
+                    instructions.Add(instruction);
+                }
             }
         }
 
         return instructions;
     }
 
-    /// <summary>
-    /// Создаёт базовый блок инструкций и возвращает ссылку на него.
-    /// </summary>
-    private BasicBlock CreateBasicBlock()
+    private bool IsJump(InstructionCode code)
     {
-        BasicBlock bb = new(_basicBlocks.Count);
-        _basicBlocks.Add(bb);
-        return bb;
+        return code switch
+        {
+            InstructionCode.Call => true,
+            InstructionCode.Jump => true,
+            InstructionCode.JumpIfFalse => true,
+            InstructionCode.JumpIfTrue => true,
+            _ => false,
+        };
+    }
+
+    private List<int> CalculateBasicBlockAddresses()
+    {
+        List<int> basicBlockAddresses = new List<int>(_basicBlocks.Count);
+        int nextBlockAddress = 0;
+
+        foreach (BasicBlock bb in _basicBlocks)
+        {
+            basicBlockAddresses.Add(nextBlockAddress);
+            nextBlockAddress += bb.Instructions.Count;
+        }
+
+        return basicBlockAddresses;
     }
 }
